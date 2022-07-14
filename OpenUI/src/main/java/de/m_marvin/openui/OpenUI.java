f5c6436525c8bb9d446df6038629f39bf14ed13c
M_Marvin <marvin.koehler@inexio.email>
2022-07-14 17:43:34 +0200

    UnifiedVectors Lib hinzugefügt

---
+++ OpenUI/src/main/java/de/m_marvin/openui/OpenUI.java
@@ -0,0 +1,5 @@
+package de.m_marvin.openui;
+
+public class OpenUI {
+
+}
---
+++ OpenUI/src/test/java/de/m_marvin/openuitest/OpenUITest.java
@@ -0,0 +1,11 @@
+package de.m_marvin.openuitest;
+
+public class OpenUITest {
+	
+	public static void main(String... args) {
+		
+		System.out.println("OpenUI test ...");
+		
+	}
+	
+}
